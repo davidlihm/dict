df9a647b96535b05966d380a5cc5e7f23d693c10
icanc <i@icanc.net>
2014-05-19 00:44:45 +0800

    add audio

--- dict.js
+++ dict.js
@@ -1,5 +1,14 @@
 var pattern = /^[a-zA-Z]{2,50}$/mg;
-window.onmouseup = function() {
+
+var dictAudio = document.createElement('audio');
+dictAudio.style.display = 'none';
+dictAudio.setAttribute('volume', 1);
+
+window.addEventListener('load', function() {
+    document.body.appendChild(dictAudio);
+});
+
+window.addEventListener('mouseup', function() {
     var text = encodeURIComponent(window.getSelection());
     text = text.replace(/\s/mg, "");
     if (!pattern.test(text)) return;
@@ -7,11 +16,17 @@ window.onmouseup = function() {
     oReq.responseType = "document";
     oReq.onload = function() {
         var result = oReq.responseXML.querySelector('acceptation');
-        if (result)
+        var audio = oReq.responseXML.querySelector('pron');
+        if (result) {
             console.log(result.innerHTML);
-        else
+            if (audio) {
+                dictAudio.setAttribute('src', audio.innerHTML);
+                dictAudio.play();
+            }
+        } else {
             console.log('not found');
+        }
     };
     oReq.open("get", 'http://dict-co.iciba.com/api/dictionary.php?key=277E4942EF0F80880C1BC1D562797AF7&w=' + text.toLowerCase());
     oReq.send();
-}
+});
